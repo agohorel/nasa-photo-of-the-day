314654645e2a5da459ff956ecf2ed84826eb94eb
agohorel <gohorelalex@gmail.com>
2019-12-11 18:23:30 -0500

    only render descr. label when finished loading

--- src/Sidebar/Description.js
+++ src/Sidebar/Description.js
@@ -4,22 +4,24 @@ import styled from "styled-components";
 export const Description = ({ description: { explanation } }) => {
   return (
     <Container>
-      <Heading>Description:</Heading>
+      {explanation ? <Heading>Description:</Heading> : null}
+
       <Content>{explanation}</Content>
     </Container>
   );
 };
 
 const Container = styled.div`
-    position: absolute;
-    bottom: 0;
-    padding: 10px;
-`
+  position: absolute;
+  bottom: 0;
+  padding: 10px;
+`;
 const Heading = styled.h3`
-    color: white;
-` 
+  color: white;
+  text-align: left;
+`;
 
 const Content = styled.p`
-    text-align: left;
-    color: #efefef;
-`
+  text-align: left;
+  color: #aaa;
+`;
