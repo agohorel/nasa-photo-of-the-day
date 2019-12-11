370b2e054782bb854ecb82b24bfef6598c944911
agohorel <gohorelalex@gmail.com>
2019-12-11 15:43:06 -0500

    add api call and render image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 import { Sidebar } from "./Sidebar/Sidebar";
@@ -7,6 +8,16 @@ import { Description } from "./Sidebar/Description";
 import { Image } from "./Image";
 
 function App() {
+  const [image, setImage] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      // .then(res => console.log(res))
+      .then(res => setImage(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div className="App">
       <Sidebar>
@@ -17,7 +28,7 @@ function App() {
           facere. Optio, quis?
         </Description>
       </Sidebar>
-      <Image></Image>
+      <Image image={image}></Image>
     </div>
   );
 }
--- src/Image.js
+++ src/Image.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-export const Image = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export const Image = ({ image: { url, title } }) => {
+  return <MainImage src={url}></MainImage>;
+};
+
+const MainImage = styled.img`
+  width: 100%;
+  height: 100vw;
+  background-color: #3c3c3c;
+`;
