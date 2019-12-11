be799edd584a0e2dee341fa589753444e1d9a10a
agohorel <gohorelalex@gmail.com>
2019-12-11 17:14:46 -0500

    add loader animation

--- src/Image.js
+++ src/Image.js
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 
+import { Loader } from "./Loader";
+
 export const Image = ({ image: { url, title } }) => {
   return (
     <ImageContainer>
-      <MainImage src={url} alt={title}></MainImage>
+      {url ? <MainImage src={url} alt={title}></MainImage> : <Loader />}
+
       {/* <MainImage
         src={
           "https://apod.nasa.gov/apod/image/1912/N63A_HubbleChandraSchmidt_960.jpg"
@@ -19,6 +22,10 @@ const ImageContainer = styled.div`
   width: calc(100% - 300px);
   height: 100vh;
   margin-left: 300px;
+  background-color: #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const MainImage = styled.img`
